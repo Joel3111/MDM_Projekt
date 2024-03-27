3ee3c3a82e94717e54e398792fe102689730ef60
Joel3111 <eglijoe2@students.zhaw.ch>
2024-03-27 15:08:12 +0100

    Anpassungen, Scraper für eine URL funktioniert so

--- scraper_Nutrition.py
+++ scraper_Nutrition.py
@@ -24,9 +24,41 @@ url = "https://naehrwertdaten.ch/de/search/#/food/340936"
 # Gehe zur URL
 driver.get(url)
 
-# Warte, damit die Seite Zeit hat, Inhalte zu laden
-time.sleep(5)
+try:
+    # Warten, bis Elemente mit der Klasse 'ui-treetable-toggler ng-star-inserted' verfügbar sind
+    elements = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "ui-treetable-toggler"))
+    )
+    time.sleep(1)
+    # Durch die gefundenen Elemente iterieren und das erste mit dem gewünschten style-Attribut anklicken
+    for element in elements:
+        style = element.get_attribute("style")
+        if "visibility: visible" in style and "margin-left: 16px" in style:
+            element.click()
+            time.sleep(3)
+            print("Gesuchtes erste Element wurde angeklickt.")
+            break
+    time.sleep(1)
+    # Warten, bis Elemente mit der Klasse 'ui-treetable-toggler ng-star-inserted' verfügbar sind
+    elements = driver.find_elements(By.CLASS_NAME, "ui-treetable-toggler")
+    
+    # Einen Zähler für passende Elemente einführen
+    match_count = 0
+    
+    # Durch die gefundenen Elemente iterieren
+    for element in elements:
+        style = element.get_attribute("style")
+        if "visibility: visible" in style and "margin-left: 16px" in style:
+            match_count += 1
+            if match_count == 2:  # Wenn das zweite passende Element gefunden wird
+                element.click()
+                print("Das zweite passende Element wurde angeklickt.")
+                break
 
+except Exception as e:
+    print("Fehler beim Suchen oder Anklicken des Elements:", e)
+
+time.sleep(1)
 # Extrahiere den Namen des Nahrungsmittels
 name_element = driver.find_element(By.CSS_SELECTOR, "label.food-content--header-title--label")
 name_des_nahrungsmittels = name_element.text.strip()
